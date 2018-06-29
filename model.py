c3ca68c0f163cb0f5d0b6caac2034127b4df33b5
Kenton Parton <kknoxparton@gmail.com>
2018-06-29 16:24:21 +0200

    updates

--- model.py
+++ model.py
@@ -60,4 +60,4 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
-model.save(model.h5)
+model.save('model.h5')
