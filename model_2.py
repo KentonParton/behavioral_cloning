b2b24c4efe91ff5013776dcaee7a9cf42657ae6b
Kenton Parton <kknoxparton@gmail.com>
2018-06-30 22:28:28 +0200

    update

--- model_2.py
+++ model_2.py
@@ -83,6 +83,7 @@ model.fit_generator(train_generator,
                     samples_per_epoch=len(train_samples),
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples),
-                    nb_epoch=3)
+                    nb_epoch=7,
+                    verbose=1)
 
 model.save('model.h5')
