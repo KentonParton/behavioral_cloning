dacccab9490d2a7d08cee6a01388dc94577a5560
Kenton Parton <kknoxparton@gmail.com>
2018-07-01 22:54:23 +0200

    update

--- model_2.py
+++ model_2.py
@@ -98,7 +98,7 @@ model.fit_generator(train_generator,
                     samples_per_epoch=len(train_samples)*2,
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples)*2,
-                    nb_epoch=7,
+                    nb_epoch=5,
                     verbose=1)
 
 model.save('model.h5')
