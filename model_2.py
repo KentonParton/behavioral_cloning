2bae9f303ca9e588821617fe43e2bb966133da29
Kenton Parton <kknoxparton@gmail.com>
2018-07-01 17:57:22 +0200

    update

--- model_2.py
+++ model_2.py
@@ -95,9 +95,9 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
 model.fit_generator(train_generator,
-                    samples_per_epoch=len(train_samples)*6,
+                    samples_per_epoch=len(train_samples)*2,
                     validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples)*6,
+                    nb_val_samples=len(validation_samples)*2,
                     nb_epoch=7,
                     verbose=1)
 
