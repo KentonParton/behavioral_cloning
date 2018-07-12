58f12dea621091570ea4aa6e4dcccfb0f3047109
Kenton Parton <kknoxparton@gmail.com>
2018-07-12 21:31:16 +0200

    6 epochs

--- model.py
+++ model.py
@@ -143,7 +143,7 @@ model.fit_generator(train_generator,
                     samples_per_epoch=len(train_samples)*2,
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples)*2,
-                    nb_epoch=3,
+                    nb_epoch=6,
                     verbose=1)
 
 # save the created trained model
